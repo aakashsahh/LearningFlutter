b4b2d828693eec0e9ed86904212d6e0dc865ecb9
aakashsahh <aakash@academiacollege.edu.np>
2023-06-29 19:39:07 +0545

    read data from local json file

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,25 +1,40 @@
- 
-class CatalogModel{
-static final items = [
-  Item(
-      id: 1,
-      name: "iPhone 12 Pro",
-      desc: "Apple iPhone 12th generation",
-      price: 999,
-      color: "#33505a",
-      image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
-];
-}
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 
-class Item {
+class Product {
   final int id;
   final String name;
   final String desc;
-  final num price;
+  final int price;
   final String color;
   final String image;
 
-  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+  Product({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.image,
+  });
 }
+
+Future<List<Product>> loadProducts() async {
+  String jsonString = await rootBundle.loadString("assets/files/catalog.json");
+  List<dynamic> data = json.decode(jsonString)['products'];
+
+  List<Product> products = data.map((productData) {
+    return Product(
+      id: productData['id'],
+      name: productData['name'],
+      desc: productData['desc'],
+      price: productData['price'],
+      color: productData['color'],
+      image: productData['image'],
+    );
+  }).toList();
+
+  return products;
+}
+
 
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:learnflutter/widgets/drawer.dart';
 
 import '../models/catalog.dart';
-import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
@@ -17,16 +21,38 @@ class HomePage extends StatelessWidget {
       ),
       drawer: const MyDrawer(),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ItemWidget(
-              item: CatalogModel.items[index],
-            );
-          },
-        ),
-      ),
+          padding: const EdgeInsets.all(16.0),
+          child: FutureBuilder<List<Product>>(
+            future: loadProducts(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<Product> products = snapshot.data!;
+
+                return ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    Product product = products[index];
+                    return Card(
+                      child: ListTile(
+                        leading: Image.network(product.image),
+                        title: Text(product.name),
+                        subtitle: Text(product.desc),
+                        trailing: Text('\$${product.price}',
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                            )),
+                      ),
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
+          )),
     );
   }
 }
